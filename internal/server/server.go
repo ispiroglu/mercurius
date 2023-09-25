3a19d5e83a4343fe83f2424d1a4f234efc166abc
Evren Ispiroglu <evrenn.ispiroglu@gmail.com>
2023-09-25 21:57:51 +0300

    Consumer worker group test

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,11 +2,14 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"runtime"
+	"sync/atomic"
+
 	"github.com/ispiroglu/mercurius/internal/broker"
 	"github.com/ispiroglu/mercurius/internal/logger"
 	"github.com/ispiroglu/mercurius/proto"
 	"go.uber.org/zap"
-	"runtime"
 )
 
 type Server struct {
@@ -28,13 +31,21 @@ func (s *Server) Publish(_ context.Context, event *proto.Event) (*proto.ACK, err
 }
 
 func (s *Server) Subscribe(req *proto.SubscribeRequest, stream proto.Mercurius_SubscribeServer) error {
-	s.logger.Info("Received subscribe request", zap.String("Topic", req.Topic))
+	// s.logger.Info("Received subscribe request", zap.String("Topic", req.Topic))
 	ctx := stream.Context()
 	sub, err := s.broker.Subscribe(ctx, req.Topic, req.SubscriberID, req.SubscriberName)
 	if err != nil {
 		s.logger.Error("Error on subscribe", zap.String("Topic", req.Topic), zap.String("SubscriberID", req.SubscriberID), zap.String("Subscriber Name", req.SubscriberName)) //, zap.Error(err))
 		return err
 	}
+
+	// This does not changes any time
+	for w := 0; w < 1; w++ {
+		go consumerTask(stream, sub, s.logger)
+	}
+
+	cc := make(chan struct{})
+	<-cc
 
 	for {
 
@@ -43,7 +54,7 @@ func (s *Server) Subscribe(req *proto.SubscribeRequest, stream proto.Mercurius_S
 
 			broker.SubscriberRetryHandler.RemoveRetryQueue(sub.Id)
 			go func(sub *broker.Subscriber) {
-				s.broker.Unsubscribe(sub)
+				// s.broker.Unsubscribe(sub)
 				runtime.GC()
 			}(sub)
 
@@ -51,17 +62,50 @@ func (s *Server) Subscribe(req *proto.SubscribeRequest, stream proto.Mercurius_S
 		case event := <-sub.EventChannel:
 			go func() {
 				if err := stream.Send(event); err != nil {
-					s.logger.Error("Error on sending event", zap.String("TopicName", event.Topic), zap.String("SubscriberID", req.SubscriberID), zap.String("Subscriber Name", req.SubscriberName)) //, zap.Error(err))
+
+					s.logger.Error("Error on sending event", zap.String("TopicName", event.Topic), zap.String("SubscriberID", sub.Id), zap.String("Subscriber Name", sub.Name)) //, zap.Error(err))
 					s.logger.Info("Sending event to retry queue")
 					sub.RetryQueue <- event
 				}
 			}()
 		}
 	}
+
+	return nil
 }
 
 func (s *Server) Retry(_ context.Context, req *proto.RetryRequest) (*proto.ACK, error) {
 	s.logger.Info("Received retry request")
 	s.broker.SubscriberRepository.Subscribers[req.SubscriberID].RetryQueue <- req.Event
 	return &proto.ACK{}, nil
+}
+
+var messageCount = atomic.Uint64{}
+
+func consumerTask(stream proto.Mercurius_SubscribeServer, sub *broker.Subscriber, logger *zap.Logger) {
+	for {
+
+		select {
+		case <-sub.Ctx.Done():
+
+			broker.SubscriberRetryHandler.RemoveRetryQueue(sub.Id)
+			go func(sub *broker.Subscriber) {
+				// s.broker.Unsubscribe(sub)
+				runtime.GC()
+			}(sub)
+
+			return
+		case event := <-sub.EventChannel:
+			fmt.Println("ASDAS", messageCount.Add(1))
+			// time.Sleep(4 * time.Second)
+			go func() {
+				if err := stream.Send(event); err != nil {
+					panic("")
+					logger.Error("Error on sending event", zap.String("TopicName", event.Topic), zap.String("SubscriberID", sub.Id), zap.String("Subscriber Name", sub.Name)) //, zap.Error(err))
+					logger.Info("Sending event to retry queue")
+					sub.RetryQueue <- event
+				}
+			}()
+		}
+	}
 }
